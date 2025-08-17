0975f696bf4af76e7e85809e2e7ca07b271b0de2
ajibadeadeleke <jibadew@gmail.com>
2025-08-17 10:05:08 +0100

    missed_payment_count metrics added to repay_metric

--- models/Intermediate/int_repayment_metrics.sql
+++ models/Intermediate/int_repayment_metrics.sql
@@ -4,7 +4,8 @@ SELECT
 	sum(expected_payment) total_payback_loan,
 	round(sum(actual_payment) / NULLIF(sum(expected_payment),0)* 100,1) percentage_paid,
 	array_agg(DISTINCT consecutive_missed_month) missed_con_month,
-    round(avg(delay_in_days)) avg_delay_days
+    round(avg(delay_in_days)) avg_delay_days,
+    count(actual_payment) FILTER (WHERE actual_payment IN (NULL, 0)) missed_payment_count
 FROM {{ref('int_repayment_con_missed_month')}}
 GROUP BY 1
 ORDER BY 1
