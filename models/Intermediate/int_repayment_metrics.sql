c5d7a292609af012bd47be956b8392916cba09b6
ajibadeadeleke <jibadew@gmail.com>
2025-08-17 10:15:27 +0100

    partial_payment_count metric added to repay_metric

--- models/Intermediate/int_repayment_metrics.sql
+++ models/Intermediate/int_repayment_metrics.sql
@@ -5,7 +5,8 @@ SELECT
 	round(sum(actual_payment) / NULLIF(sum(expected_payment),0)* 100,1) percentage_paid,
 	array_agg(DISTINCT consecutive_missed_month) missed_con_month,
     round(avg(delay_in_days)) avg_delay_days,
-    count(actual_payment) FILTER (WHERE actual_payment IN (NULL, 0)) missed_payment_count
+    count(actual_payment) FILTER (WHERE actual_payment IN (NULL, 0)) missed_payment_count,
+    count(actual_payment) FILTER (WHERE actual_payment > 0 AND actual_payment < expected_payment) AS partial_payment_count
 FROM {{ref('int_repayment_con_missed_month')}}
 GROUP BY 1
 ORDER BY 1
